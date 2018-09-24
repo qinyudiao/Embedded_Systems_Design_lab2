54a9acbccc6b4e2b6987860bc402704ba3fe5f47
Qinyu Diao <qdiao@utexas.edu>
2018-09-24 16:21:35 -0500

    lab2

---
+++ Lab2/adctestmain.d
@@ -0,0 +1,7 @@
+.\adctestmain.o: ADCTestMain.c
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adctestmain.o: ST7735.h
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\limits.h
+.\adctestmain.o: ADCSWTrigger.h
+.\adctestmain.o: tm4c123gh6pm.h
+.\adctestmain.o: PLL.h
